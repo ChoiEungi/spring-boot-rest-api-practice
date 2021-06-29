eddab1240b12f7ccdda17e4ebbceb81574a5539c
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-06-29 15:20:44 +0900

    until pathvariable

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.example.restfulwebservice;
+// lombok : setter getter auto making
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // getter and setter
+@AllArgsConstructor // constructor
+@NoArgsConstructor // default constructor
+public class HelloWorldBean {
+    private String message;
+
+//    public HelloWorldBean(String message){
+//        this.message = message;
+//    } -> duplicate lombok AllArgsConstructor, so don't need to create constructor
+
+}
