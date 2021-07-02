7b00403dced71b78166f65c78b9e4b275f4078f0
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-02 18:01:47 +0900

    implement delete method

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -45,6 +45,15 @@ public class UserController {
 
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+
+        if (user==null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+    }
+
 
 
 }
