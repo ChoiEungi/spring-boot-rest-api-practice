b17f9382315f86376377b9d062dabd7b53aff8d6
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-04 21:15:58 +0900

    api version control by using mime-type

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -39,7 +39,8 @@ public class AdminUserController {
 
     // GET /admin/users/1 -> /admin/v1/users/1
 //    @GetMapping("/v1/users/{id}")
-    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = service.findOne(id);
 
@@ -60,7 +61,8 @@ public class AdminUserController {
     }
 
 //    @GetMapping("/v2/users/{id}")
-    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = service.findOne(id);
 
