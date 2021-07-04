c1f64acae0296cb56fa394f6579b0d1114dba964
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-04 19:32:15 +0900

    filtering by using @JsonFilter

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,7 +1,7 @@
 package com.example.restfulwebservice.user;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +12,8 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-@JsonIgnoreProperties(value = {"password", "ssn"}) // ignore the json value in array
+//@JsonIgnoreProperties(value = {"password", "ssn"}) // ignore the json value in array
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
 
@@ -24,4 +25,5 @@ public class User {
 
     private String password;
     private String ssn;
+
 }
