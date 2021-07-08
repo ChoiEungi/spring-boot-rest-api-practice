66e1471ebe41ea0742180c1281b1948ae3333083
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-08 20:07:02 +0900

    add sql

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -4,16 +4,26 @@ package com.example.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration // loading setting info
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-        throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("eungi")
-                .password("{noop}pass1234")
-                .roles("USER");
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
     }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth)
+//        throws Exception{
+//        auth.inMemoryAuthentication()
+//                .withUser("eungi")
+//                .password("{noop}pass1234")
+//                .roles("USER");
+//    }
 }
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,10 +3,17 @@ package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -16,16 +23,26 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password", "ssn"}) // ignore the json value in array
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "두 글자 이상을 입력해주세요.")
+    @ApiModelProperty(notes = "")
     private String name;
     
     @Past
+    @ApiModelProperty(notes = "")
     private Date joinDate;
 
+    @ApiModelProperty(notes = "")
     private String password;
+
+    @ApiModelProperty("")
     private String ssn;
 
 }
