5b80a991d58970d1ec2171bd6f35d9f14abb459a
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-11 01:33:53 +0900

    add posts by http post method

--- src/main/java/com/example/restfulwebservice/user/Post.java
+++ src/main/java/com/example/restfulwebservice/user/Post.java
@@ -7,10 +7,7 @@ import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Data //setter getter
 @Entity
@@ -19,7 +16,7 @@ import javax.persistence.ManyToOne;
 public class Post {
 
     @Id
-    @Generated
+    @GeneratedValue
     private Integer id;
 
     private String description;
